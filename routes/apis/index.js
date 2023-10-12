50400bc41824b62cde8f6c05f069229e6d25554f
Peter-Haung888 <ming31437@gmail.com>
2023-10-12 15:36:43 +0800

    feat: add signUp api

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const passport = require('../../config/passport')
-const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth') // 新增這裡
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 
 const restController = require('../../controllers/apis/restaurant-controller')
 const userController = require('../../controllers/apis/user-controller')
@@ -10,6 +10,7 @@ const { apiErrorHandler } = require('../../middleware/error-handler')
 
 router.use('/admin', authenticated, authenticatedAdmin, admin)
 router.get('/restaurants', authenticated, restController.getRestaurants)
+router.post('/signup', userController.signUp)
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 
 router.use('/', apiErrorHandler)
